1202d97f0278c783b2502249079d810dc8d0a7cd
Vladimir Sotskov <bruh@shitposting.expert>
2024-10-30 19:54:43 +0300

    modify build script to use msvc cl.exe

--- build.bat
+++ build.bat
@@ -7,14 +7,25 @@ if "%~1"=="" goto :build
 :: Handle different commands
 if "%~1"=="build" goto :build
 if "%~1"=="run" goto :run
+if "%~1"=="MSbuild" goto :msbuild
 
 echo Unknown command: %~1
 echo Usage: build.bat [build^|run]
 exit /b 1
 
-:build
+:msbuild
 echo Using MSBuild
 "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" projects/VS2022
+if errorlevel 1 (
+    echo Build failed
+    exit /b 1
+)
+".\projects\VS2022\build\raylib_game\bin\x64\Debug\raylib_game.exe"
+goto :eof
+
+:build
+echo Building game using cl.exe
+cl src/raylib_game.c /I"../raylib-5.0_win64_msvc16/include/" /MD /link /MACHINE:X64 /OUT:"The_Apprentice.exe" "../raylib-5.0_win64_msvc16/lib/raylib.lib" opengl32.lib kernel32.lib user32.lib shell32.lib gdi32.lib winmm.lib msvcrt.lib
 if errorlevel 1 (
     echo Build failed
     exit /b 1
@@ -23,7 +34,8 @@ goto :eof
 
 :run
 echo Using MSBuild
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" projects/VS2022
+rem "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" projects/VS2022
+cl src/raylib_game.c /I"../raylib-5.0_win64_msvc16/include/" /MD /link /MACHINE:X64 /OUT:"The_Apprentice.exe" "../raylib-5.0_win64_msvc16/lib/raylib.lib" opengl32.lib kernel32.lib user32.lib shell32.lib gdi32.lib winmm.lib msvcrt.lib
 if errorlevel 1 (
     echo Build failed
     exit /b 1
@@ -32,4 +44,4 @@ echo --------------------------------------------------------------
 echo Running Game
 echo --------------------------------------------------------------
 
-".\projects\VS2022\build\raylib_game\bin\x64\Debug\raylib_game.exe"
+.\The_Apprentice.exe
